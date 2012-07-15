9d408ad6471dcc14cf86deb4b6639dfed71f44ae
squirrelsoftjp <squirrelsoftjp@gmail.com>
2012-07-15 16:59:06 +0900

    setTheme

--- AndroidDemo/src/com/example/androiddemo/MainActivity.java
+++ AndroidDemo/src/com/example/androiddemo/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setVisible(false);
+        setTheme(android.R.style.Theme_Black);
     }
 
     @Override
